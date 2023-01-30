42e2a050a752a10115135a9e626ff63a444eb3a6
Shivam Singh <shivam02bhu@gmail.com>
2023-01-30 22:55:28 +0530

    Modified car

--- car.java
+++ car.java
@@ -1,121 +1,45 @@
-
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.*;
 
 public class car {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        AutonomousCar[] a = new AutonomousCar[4];
+        int carId[] = new int[4];
+        String brand[] = new String[4];
+        int noOfTestsConducted[] = new int[4];
+        int noOfTestsPassed[] = new int[4];
+        String environment[] = new String[4];
         for (int i = 0; i < 4; i++) {
-            a[i].setCarId(sc.nextInt());
-            a[i].setBrand(sc.nextLine());
-            a[i].setNoOfTestsConducted(sc.nextInt());
-            // sc.nextLine();
-            a[i].setNoOfTestsPassed(sc.nextInt());
-            // sc.nextLine();
-            a[i].setEnvironment(sc.nextLine());
+            carId[i] = sc.nextInt();
+            sc.nextLine();
+            brand[i] = sc.nextLine();
+            noOfTestsConducted[i] = sc.nextInt();
+            sc.nextLine();
+            noOfTestsPassed[i] = sc.nextInt();
+            sc.nextLine();
+            environment[i] = sc.nextLine();
         }
         String e = sc.nextLine();
         String b = sc.nextLine();
         sc.close();
-        int testPassed = findTestPassedByEnv(a, e);
-        if (testPassed >= 0) {
-            System.out.println("There are no tests passed in this particular environment");
-        }
-        AutonomousCar ac = updateCarGrade(b, a);
-        System.out.println(ac.getBrand() + "::" + ac.getGrade());
-    }
-
-    public static int findTestPassedByEnv(AutonomousCar[] ac, String env) {
         int sum = 0;
         for (int i = 0; i < 4; i++) {
-            if (ac[i].getEnvironment() == env) {
-                sum += ac[i].getTestsPassed();
+            if (environment[i].equals(e)) {
+                sum += noOfTestsPassed[i];
             }
         }
-        return sum;
-    }
-
-    public static String findGrade(int passed, int conducted) {
-        int rating = (passed * 100) / (conducted);
-        if (rating >= 80) {
-            return "A1";
+        if(sum == 0) {
+            System.out.println("There are no tests passed in this particular environment");
         } else {
-            return "B2";
+            System.out.println(sum);
         }
-    }
-
-    public static AutonomousCar updateCarGrade(String brand, AutonomousCar[] ac) {
-        AutonomousCar bc = new AutonomousCar();
         for (int i = 0; i < 4; i++) {
-            if (brand == ac[i].getBrand()) {
-                ac[i].setGrade(findGrade(ac[i].getTestsPassed(), ac[i].getTestsConducted()));
-                bc = ac[i];
+            if (brand[i].equals(b)) {
+                int rating = (noOfTestsPassed[i]*100)/(noOfTestsConducted[i]);
+                if(rating >= 80)
+                    System.out.println(brand[i]+"::A1");
+                else
+                    System.out.println(brand[i]+"::B2");                
             }
         }
-        return bc;
-    }
-}
-
-class AutonomousCar {
-    int carId;
-    String brand;
-    int noOfTestsConducted;
-    int noOfTestsPassed;
-    String environment;
-    String grade;
-
-    void setCarId(int id) {
-        carId = id;
-    }
-
-    int getCarId() {
-        return carId;
-    }
-
-    void setBrand(String b) {
-        brand = b;
-    }
-
-    String getBrand() {
-        return brand;
-    }
-
-    void setNoOfTestsConducted(int t) {
-        noOfTestsConducted = t;
-    }
-
-    int getTestsConducted() {
-        return noOfTestsConducted;
-    }
-
-    void setNoOfTestsPassed(int t) {
-        noOfTestsPassed = t;
-    }
-
-    int getTestsPassed() {
-        return noOfTestsPassed;
-    }
-
-    void setEnvironment(String env) {
-        environment = env;
-    }
-
-    String getEnvironment() {
-        return environment;
-    }
-
-    void setGrade(String g) {
-        grade = g;
-    }
-
-    String getGrade() {
-        return grade;
     }
 }
